9bebb309bf25c1038e361c6b6a6300e7ea612c5f
Deniz Isikli <s215818@dtu.dk>
2026-01-03 01:34:01 +0100

    Refactor and added runtime selection of ffmpeg or normal version

--- LindenmayerSystem.cpp
+++ LindenmayerSystem.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 #include <SDL2/SDL.h>
+#include <unistd.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 #define LENGTH 5
+#define FPS 60
+#define RECORD_SECONDS 1.25
 
 constexpr double ANGLE = 0.2 * M_PI;
 
@@ -11,7 +14,7 @@ size_t drawIndex = 0;
 int MAX_BRANCHING = 8;
 std::string axiom = "F";
 
-double x,y,angle;
+double x, y, angle;
 std::stack<std::tuple<double,double,double>> stack;
 
 Uint32 finishedTime = 0;
@@ -56,7 +59,7 @@ void resetBranching() {
     while (!stack.empty()) stack.pop();
 }
 
-int main() {
+void LSystem() {
     SDL_Init(SDL_INIT_VIDEO);
     srand(static_cast<unsigned>(time(nullptr)));
 
@@ -93,7 +96,7 @@ int main() {
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
         int stepsPerFrame = 50;
-        
+
         for (int i = 0; i < stepsPerFrame && drawIndex < axiom.size(); i++, drawIndex++) {
             char c = axiom[drawIndex];
             switch (c) {
@@ -121,7 +124,7 @@ int main() {
         }
 
         if (drawIndex >= axiom.size()) {
-            if(finishedTime == 0) {
+            if (finishedTime == 0) {
                 finishedTime = SDL_GetTicks();
             }
 
@@ -138,5 +141,93 @@ int main() {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+void LSystemFfmpeg() {
+    SDL_Init(SDL_INIT_VIDEO);
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    SDL_Window* window = SDL_CreateWindow(
+        "L-System (FFmpeg)",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    resetBranching();
+
+    std::vector<uint8_t> pixels(WINDOW_WIDTH * WINDOW_HEIGHT * 4);
+
+    const int total_frames = FPS * RECORD_SECONDS;
+    for (int frame = 0; frame < total_frames; frame++) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) return;
+        }
+
+        if (drawIndex == 0) {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderClear(renderer);
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        int stepsPerFrame = 50;
+
+        for (int i = 0; i < stepsPerFrame && drawIndex < axiom.size(); i++, drawIndex++) {
+            char c = axiom[drawIndex];
+            switch (c) {
+                case 'F': {
+                    double nx = x + LENGTH * cos(angle);
+                    double ny = y + LENGTH * sin(angle);
+                    SDL_RenderDrawLine(renderer, x, y, nx, ny);
+                    x = nx;
+                    y = ny;
+                    break;
+                }
+                case '+': angle += ANGLE; break;
+                case '-': angle -= ANGLE; break;
+                case '[': stack.push({x, y, angle}); break;
+                case ']':
+                    if (!stack.empty()) {
+                        auto [px, py, pa] = stack.top();
+                        stack.pop();
+                        x = px;
+                        y = py;
+                        angle = pa;
+                    }
+                    break;
+            }
+        }
+
+        SDL_RenderPresent(renderer);
+
+        SDL_RenderReadPixels(
+            renderer,
+            nullptr,
+            SDL_PIXELFORMAT_RGBA32,
+            pixels.data(),
+            WINDOW_WIDTH * 4
+        );
+
+        write(STDOUT_FILENO, pixels.data(), pixels.size());
+        SDL_Delay(1000 / FPS);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char** argv) {
+    if (argc > 1 && std::string(argv[1]) == "ffmpeg") {
+        LSystemFfmpeg();
+    } else {
+        LSystem();
+    }
     return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# Animated L-System
+# Lindenmayer System
 
-A simple SDL2 + C++ program that animates tree-like L-systems using turtle graphics.
+A simple SDL2 + C++ program that animates tree-like **L-systems** using turtle graphics.
+
+<p align="center">
+  <img src="LindenmayerSystem.gif" alt="Animated L-System Demo" />
+</p>
+
+## Compile
+
+```bash
+g++ LindenmayerSystem.cpp -std=c++17 -O2 `sdl2-config --cflags --libs` -o LindenmayerSystem
+```
+
+## Run
+
+```bash
+# Normal interactive mode
+./LindenmayerSystem
+```
+
+```bash
+# FFmpeg capture mode (GIF)
+./LindenmayerSystem ffmpeg | ffmpeg -f rawvideo -pixel_format rgba -video_size 800x600 -framerate 60 -i - LindenmayerSystem.gif
+```
